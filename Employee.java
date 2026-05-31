e2aedb388fb77fb157a7fdf19f9f8cadb7ac2ae0
Krish56920 <srikrishna584101@gmail.com>
2026-05-31 23:13:15 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,18 +1,19 @@
 package com.Employee.Management;
+
 public class Employee {
 
     private int id;
     private String name;
-    private String department;
     private double salary;
 
-    public Employee(int id, String name, String department, double salary) {
+    // Constructor
+    public Employee(int id, String name, double salary) {
         this.id = id;
         this.name = name;
-        this.department = department;
         this.salary = salary;
     }
 
+    // Getter methods
     public int getId() {
         return id;
     }
@@ -21,23 +22,15 @@ public class Employee {
         return name;
     }
 
-    public String getDepartment() {
-        return department;
-    }
-
     public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee ID: " + id +
-                ", Name: " + name +
-                ", Department: " + department +
-                ", Salary: " + salary;
+    // Display employee details
+    public void display() {
+        System.out.println("Employee ID : " + id);
+        System.out.println("Employee Name : " + name);
+        System.out.println("Employee Salary : " + salary);
+        System.out.println("--------------------------");
     }
 }
